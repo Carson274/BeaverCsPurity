d2b8807400095dc44a7c6e8787549b1dc4e491e1
Carson Secrest <carsonsecrest@gmail.com>
2025-05-27 13:14:50 -0700

    feat: configure routing with crossroad library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import HomePage from './pages/HomePage'
+import ResultsPage from './pages/ResultsPage'
 import './App.css'
+import Router, { Switch, Route } from 'crossroad';
+
 
 function App() {
   const [score, setScore] = useState(100)
+  const Home = () => <HomePage setScore={setScore} />
+  const Results = () => <ResultsPage score={score} />
 
   return (
     <div className='app-div'>
-      <HomePage score={score} setScore={setScore}/>
+      <Router>
+        <Switch>
+          <Route path='/' component={Home} />
+          <Route path='/results' component={Results} />
+        </Switch>
+      </Router>
     </div>
   )
 }
