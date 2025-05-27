173050459b93289d3e0c440d8870a85b32090324
Carson Secrest <carsonsecrest@gmail.com>
2025-05-26 23:42:24 -0700

    feat: create initial footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <p className='footer-text p-text'>&copy; Oregon State Purity Test 2025</p>
+      <p className='footer-text p-text'>Made with ❤️ by 
+        <a className='orange-text bold-text' href='https://www.linkedin.com/in/carson-secrest' target='_blank'> Carson</a>
+      </p>
+    </div>
+  )
+}
