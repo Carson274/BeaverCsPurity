249614d709de8f06c1cc72ef570f91ac8ed402b0
Carson Secrest <carsonsecrest@gmail.com>
2025-06-03 11:28:04 -0700

    feat: update result messages

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -4,17 +4,13 @@ import type { MessageItem } from '../types'
 export default function ResultsPage({ score }: { score: number }) {
   const messages: MessageItem[] = [
     { floor: 100, text: "Not a single one? Are you sure you're a CS major?" },
-    { floor: 90, text: "You must either be a freshman or have a social life. Congrats! Goodluck finding a j*b though." },
-    { floor: 80, text: "You're still mostly pure. Still time to fall." },
-    { floor: 70, text: "You've seen some stuff." },
+    { floor: 80, text: "You must either be a freshman or have a social life. Congrats! Goodluck finding a j*b though." },
+    { floor: 70, text: "Whoops! Looks like you're starting to fit in a little too much. You may still have hope, but it's not looking good. Your EQ is probably dropping by the hour." },
     { floor: 69, text: "Nice." },
-    { floor: 60, text: "You’ve begun the descent into chaos." },
-    { floor: 50, text: "You’re halfway to morally bankrupt." },
-    { floor: 40, text: "Things are getting concerning." },
-    { floor: 30, text: "Are you okay?" },
-    { floor: 20, text: "Okay teacher's pet." },
-    { floor: 10, text: "If you're this low and still don't have a j*b yet, I'm doomed." },
-    { floor: 1, text: "You are a disgrace to the beaver community." },
+    { floor: 50, text: "Whoops! Looks like you're starting to fit in a little too much. You may still have hope, but it's not looking good. Your EQ is probably dropping by the hour." },
+    { floor: 30, text: "In case it wasn't clear, a higher score is better buddy. You've basically reached peak critter and it's not looking good. You're probably that guy wearing sandals without socks in Kelley." },
+    { floor: 15, text: "Either you cheated or you're actually going to be single forever. You probably think coffee-chats are dates and I bet you only shower when you've exercised." },
+    { floor: 5, text: "If you're this low and still don't have a j*b yet, I'm doomed." },
     { floor: 0, text: "Holy critter." },
   ];
 
